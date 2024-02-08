bc23d052e4a8ae79a3197680ab4228316ac447b0
Clownxz <Fatestheadgamin@gmail.com>
2024-02-08 02:42:05 -0500

    Bump v0.7.0

--- example/matter/src/shared/routes.lua
+++ example/matter/src/shared/routes.lua
@@ -4,15 +4,17 @@ local Packages = ReplicatedStorage.Packages
 type ComponentInstance<T> = T & { patch: (self: T, data: { any: any }) -> ComponentInstance<T> }
 
 local Net = require(Packages.Net)
-type Net<U...> = Net.Net<U...>
 
-local defaultConfiguration = {
-	Channel = "Unreliable",
+local Route = Net.Route
+type Route<U...> = Net.Route<U...>
+
+local defaultConfiguration: Net.Configuration = {
+	Channel = "Reliable",
 	Event = "default",
 }
 
 -- Replicate Matter Components
-local MatterReplication: Net<{ ComponentInstance<any> }> = Net.new(defaultConfiguration)
+local MatterReplication: Route<{ ComponentInstance<any> }> = Route.new(defaultConfiguration)
 
 return {
 	MatterReplication = MatterReplication,
